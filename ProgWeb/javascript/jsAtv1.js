b086332d172a4922700354752c72e940511066c8
harianadm <91471333+harianadm@users.noreply.github.com>
2023-04-10 10:25:07 -0300

    Delete ProgWeb directory

--- ProgWeb/javascript/jsAtv1.js
+++
@@ -1,9 +0,0 @@
-let idadeAtual = prompt("Idade atual: ")
-let idadeMax = prompt("Idade Máxima: ");
-let lanchePerDia = prompt("Lanches por dia:");
-let lanchesAno = lanchePerDia*365;
-
-let idadeVelhice = idadeMax - idadeAtual;
-let lanchesVelhice = idadeVelhice*lanchesAno;
-
-document.writeln("Você precisará de " + lanchesVelhice +  " 17para durar até a velhice de " + idadeVelhice+ " anos");
--- ProgWeb/javascript/jsAtv4.js
+++
@@ -1,5 +0,0 @@
-let padrao = ""
-for(let i = 1; i<9;i++){
-    padrao += `${i}`
-    document.writeln(padrao + "<br>")
-}
